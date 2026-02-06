309134f6097ec6679b1d75fa586e23268ad4a74e
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2026-02-06 23:27:19 +0530

    feat: create User model at models/User.js

---
+++ backend/models/User.js
@@ -0,0 +1,36 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING(150),
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    confirmPassword: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "users",
+    timestamps: true,
+    underscored: true,
+  },
+);
+
+export default User;
