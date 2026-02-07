609af1f611f04cb0a5db394face8086b78cc59da
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2026-02-07 22:59:29 +0530

    fetch: create navbar component and add into App.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/RegistrationPage";
+import Dashboard from "./pages/Dashboard";
 import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<SignupPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </>
   );
