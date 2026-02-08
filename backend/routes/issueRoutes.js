25daee6a6c894ae58d9dea042d9c1f9a4457a54d
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2026-02-08 17:13:36 +0530

    feat: add route to addIssue and create function

--- backend/routes/issueRoutes.js
+++ backend/routes/issueRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { fetchIssues } from "../controllers/issueController.js";
+import { fetchIssues, addIssue } from "../controllers/issueController.js";
 const router = express.Router();
 
 router.get("/fetch", fetchIssues);
+router.post("/add", addIssue);
 export default router;
