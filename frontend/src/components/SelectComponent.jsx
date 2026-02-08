6e17ddcfcd8f521fb32409a832b919c14dcfa0a6
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2026-02-09 00:28:10 +0530

    fix: bug fix - not updating filters

--- frontend/src/components/SelectComponent.jsx
+++ frontend/src/components/SelectComponent.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-function SelectComponent({ filter, handleChange, name, options }) {
+function SelectComponent({ filter, onChange, name, options }) {
   return (
     <>
       <select
         className="mb-4 p-2 rounded border border-gray-300 mr-4"
         value={filter}
         name={name}
-        onChange={handleChange}
+        onChange={onChange}
       >
         {options.map((option) => (
           <option key={option.value} value={option.value}>
