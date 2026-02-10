f4ea79e515aef69b596da4ec3483ddc4d878196d
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2026-02-11 01:18:48 +0530

    feat: modify ui of cards with dynamic styling

--- frontend/src/components/Analytics.jsx
+++ frontend/src/components/Analytics.jsx
@@ -48,16 +48,19 @@ function Analytics() {
           title="Open Issues"
           count={statusCounts.Open}
           color="bg-green-100"
+          fColor="text-green-400"
         />
         <Card
           title="In Progress"
           count={statusCounts["In Progress"]}
           color="bg-yellow-100"
+          fColor="text-yellow-400"
         />
         <Card
           title="Resolved"
           count={statusCounts.Resolved}
           color="bg-blue-100"
+          fColor="text-blue-400"
         />
       </div>
     </>
