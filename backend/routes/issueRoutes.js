b04fae987de62d7eb4d5e2679a584f472756bb16
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2026-02-10 11:22:40 +0530

    feat: create authenticateToken function to authenticate tokens and add with all api end points

--- backend/routes/issueRoutes.js
+++ backend/routes/issueRoutes.js
@@ -5,11 +5,12 @@ import {
   deleteIssue,
   updateIssue,
 } from "../controllers/issueController.js";
+import authenticateToken from "../middleware/authenticateToken.js";
 const router = express.Router();
 
-router.get("/fetch", fetchIssues);
-router.post("/add", addIssue);
-router.delete("/delete/:id", deleteIssue);
-router.put("/update/:id", updateIssue);
+router.get("/fetch", authenticateToken, fetchIssues);
+router.post("/add", authenticateToken, addIssue);
+router.delete("/delete/:id", authenticateToken, deleteIssue);
+router.put("/update/:id", authenticateToken, updateIssue);
 
 export default router;
