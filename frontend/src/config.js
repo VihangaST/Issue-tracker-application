47698914787a6c2b323edd728e036c1934bb7df2
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2026-02-11 20:43:48 +0530

    fix: modify config.js to handle url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,4 @@
 // src/config.js
 
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+export const BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
