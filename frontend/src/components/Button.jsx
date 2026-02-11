046b696399a0901731db03bf2f997c433f269ea2
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2026-02-12 00:50:28 +0530

    feat: modify Button component to allow color prop

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 
-function Button({ onClickFunction, name }) {
+function Button({ onClickFunction, name, color }) {
+  const colorClasses = {
+    cyan1: "bg-cyan-700 hover:bg-cyan-600",
+    cyan2: "bg-cyan-500 hover:bg-cyan-400",
+    gray: "bg-gray-700 hover:bg-gray-600",
+  };
+  const selectedColor = colorClasses[color] || colorClasses.cyan;
   return (
-    <>
-      <button
-        className="mb-4 rounded bg-cyan-700 text-white font-semibold hover:bg-cyan-600 w-40 h-8"
-        onClick={onClickFunction}
-      >
-        {name}
-      </button>
-    </>
+    <button
+      className={`mb-4 rounded text-white font-semibold w-40 h-8 ${selectedColor}`}
+      onClick={onClickFunction}
+    >
+      {name}
+    </button>
   );
 }
 
